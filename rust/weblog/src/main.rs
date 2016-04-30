3db907f1484f478ce79c28247c049ff3d6af5032
FoxBoxPDX <foxboxpdx@gmail.com>
2016-04-29 22:23:05 -0700

    lol i dunno did i do anything

--- rust/weblog/src/main.rs
+++ rust/weblog/src/main.rs
@@ -26,10 +26,6 @@ fn main() {
         // Else
         _ => println!("Unknown argument: {}", action),
     }
-    // Example line here
-    //let example = "127.0.0.1 - frank [10/Oct/2000:13:55:36 -0700] \"GET /apache_pb.gif HTTP/1.0\" 200 2326 \"http://www.example.com/start.html\" \"Mozilla/4.08 [en] (Win98; I ;Nav)\"";
-    //let v = weblog::parser::parse_weblog(example);
-    //println!("It's a weblog line! {}", v);
 }
 
 fn do_parse(filename: &str) {
@@ -48,6 +44,8 @@ fn do_parse(filename: &str) {
         let v = weblog::parser::parse_weblog(&l);
         weblogs.push(v);
     }
+
+    println!("Processed {} records.", weblogs.len());
 }
 
 fn do_fetch(records: &str) {
